b0e03f674112e59f468df6549032de7a7d3f019f
melonknee <140412742+melonknee@users.noreply.github.com>
2025-06-12 15:30:55 +0000

    basic frontend

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+# from annotated_text import annotated_text
+from datetime import datetime, time, timedelta
+
+import torch
+import numpy as np
+
+# from utils import *
+# from model.architectures import *
+# from model.model_loader import load_model
+
+import requests
+
+def main():
+
+    st.title("Hacker News Upvote Predictor 🚀")
+
+    input_container = st.container()
+    with input_container:
+
+        st.write("Enter post details below to predict the number of upvotes.")
+
+        default_date = datetime.now().date()
+        default_time = datetime.now()+timedelta(hours=1)
+        # st.write(default_time.strftime("%H:%M"))
+
+        title = st.text_input("Post Title")
+        author = st.text_input("Author")
+        url = st.text_input("URL Link Attached")
+        date = st.date_input("Post Date", value=default_date)
+        time_str = st.text_input("Post Time (HH:MM)", value=default_time.strftime("%H:%M"))
+
+        
+        # Combine into a single datetime object if needed
+        # full_datetime = datetime.combine(date, timee)
+        st.write(f"You selected: {date} @ {time_str}")
+
+        if st.button("Predict"):
+            try:
+                entered_time = datetime.strptime(time_str, "%H:%M").time()
+                st.success(f"You selected: {date} @ {time_str}")
+                input_data = {
+                    "title": title,
+                    "url": url,
+                    "user": author,
+                    # "date": date,
+                    "timestamp": time_str
+                }
+            except ValueError:
+                st.error("Please enter time in 24-hour HH:MM format.")
+            # prediction = model.predict(process_input(input_data))
+            # st.metric("Predicted Upvotes", round(prediction[0], 2))
+
+            try:
+                response = requests.post("http://localhost:8000/predict", json=input_data)
+
+                if response.status_code == 200:
+                    prediction = response.json()["prediction"]
+                    st.success(f"Predicted Upvotes: {prediction}")
+                else:
+                    st.error(f"Error from server: {response.text}")
+            except Exception as e:
+                st.error(f"Failed to connect to FastAPI: {e}")
+main()
