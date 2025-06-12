d7e6d12987244f389361a381131149815d7d777d
James Carter <jamesascarter@gmail.com>
2025-06-12 16:30:38 +0100

    dcscd

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ app = FastAPI()
 @app.post("/predict")
 def predict(title: str):
     # Load the model
-    model = load_model()
+    #model = load_model()
 
     # Preprocess the title
-    title_embedding = preprocess_title(title)
+    #title_embedding = preprocess_title(title)
 
     # Make a prediction
-    prediction = model.predict(title_embedding) 
+    #prediction = model.predict(title_embedding) 
 
     # Return the prediction
-    return {"prediction": prediction}
+    return {"prediction": 'prediction'}
 
 def load_model():
     return None
